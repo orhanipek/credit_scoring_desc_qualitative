9d0835e4e6c401fe00b4df04501bd650db4b95dc
YousufSiddiqui <siddiquy@student.hu-berlin.de>
2016-06-22 15:12:16 +0200

    amended code

--- QualitativeBar/Bar_Plot.R
+++ QualitativeBar/Bar_Plot.R
@@ -1,8 +1,22 @@
-###BarPlots###
-
-?barplot() 
+library(formatR)
+tidy_source(file = "Bar_Plotoutput.R")
 graphics.off()
 
+C_D = read.table('german.data')
+
+ncol(C_D)
+nrow(C_D)
+
+#Setting Variable Names
+colnames(C_D) = c("Account_Status","Months","Credit_History","Credit_Purpose","Credit_Amount","Savings_Account"
+                  ,"Employed_Since","Installment_Rate","Status_and_Sex","Other_Debtors","Residence_Since",
+                  "Property","Age","Other_Installement_Plans","Housing_Type","Existing_Credit","Job_Type",
+                  "Dependents","Telephone","Foreign_Worker","Cost_Matrix")
+
+
+
+
+###BarPlots###
 
 par(mfrow=c(2,3))
 
